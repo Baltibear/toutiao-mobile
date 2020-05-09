e820ad381f91620f60d46bfc142d909f77912869
Baltibear <530162294@qq.com>
2020-05-09 19:19:09 +0800

    第一天

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <h1>ahaha</h1>
-    <router-view />
-    <van-button type="default">默认按钮</van-button>
-<van-button type="primary">主要按钮</van-button>
+   <router-view />
   </div>
 </template>
 <script>
---
+++ src/views/login/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar
+  class="app-nav-bar"
+  title="登录"
+  left-arrow
+  @click-left="$router.back()"
+/>
+
+<van-cell-group>
+  <van-field
+    v-model="user.mobile"
+    left-icon="iconshouji"
+    placeholder="请输入手机号"
+  />
+  <van-field
+    v-model="user.code"
+    clearable
+    left-icon="iconyanzhengma"
+    placeholder="请输入验证码"
+>
+ <template #button>
+    <van-button class="send-btn" size="small" round>发送验证码</van-button>
+  </template>
+</van-field>
+<div class="login-btn-wrap">
+  <van-button class="login-btn" type="info" block @click="onLogin">登录</van-button>
+</div>
+</van-cell-group>
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/user'
+
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    async onLogin () {
+      try {
+        const res = await login(this.user)
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+        console.log('登陆失败', err)
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+  .login-container{
+    .send-btn{
+      width: 82px;
+      height: 23px;
+      background-color: #ededed;
+      .van-button__text {
+        font-size: 11px;
+        color: #666666;
+      }
+    }
+    .login-btn-wrap{
+      padding: 26px 16px;
+      .login-btn {
+        background-color: #6db4fb;
+        border: none;
+        .van-button__text {
+          font-size: 15px;
+        }
+      }
+    }
+  }
+</style>
