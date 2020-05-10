30948e9b3f91f2b95f3a8d1410241d00e36415af
Baltibear <530162294@qq.com>
2020-05-10 10:33:58 +0800

    验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,12 +7,13 @@
   @click-left="$router.back()"
 />
 
-<van-cell-group>
+<van-form @submit="onLogin">
   <van-field
     v-model="user.mobile"
     icon-prefix="icon"
     left-icon="shouji"
     placeholder="请输入手机号"
+    :rules="formRules.mobile"
   />
   <van-field
     v-model="user.code"
@@ -20,20 +21,22 @@
     icon-prefix="icon"
     left-icon="yanzhengma"
     placeholder="请输入验证码"
+    :rules="formRules.code"
 >
  <template #button>
     <van-button class="send-btn" size="small" round>发送验证码</van-button>
   </template>
 </van-field>
 <div class="login-btn-wrap">
-  <van-button class="login-btn" type="info" block @click="onLogin">登录</van-button>
+  <van-button class="login-btn" type="info" block >登录</van-button>
 </div>
-</van-cell-group>
+</van-form>
   </div>
 </template>
 
 <script>
 import { login } from '@/api/user'
+import { Toast } from 'vant'
 
 export default {
   name: 'LoginIndex',
@@ -44,6 +47,16 @@ export default {
       user: {
         mobile: '',
         code: ''
+      },
+      formRules: {
+        mobile: [
+          { required: true, message: '请填写手机号' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '请填写验证码' },
+          { pattern: /^\d{6}$/, message: '验证码错误' }
+        ]
       }
     }
   },
@@ -53,12 +66,18 @@ export default {
   mounted () {},
   methods: {
     async onLogin () {
+      Toast.loading({
+        message: '登录中...',
+        forbidClick: true,
+        duration: 0
+      })
       try {
         const res = await login(this.user)
         console.log(res)
+        Toast.success('登录成功')
       } catch (err) {
         console.log(err)
-        console.log('登陆失败', err)
+        Toast.fail('登陆失败，手机号或验证码错误')
       }
     }
   }
